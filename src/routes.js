243bae35659556f2bd20b366d78e8f7967ce98af
dankata3 <32322312+dankata3@users.noreply.github.com>
2019-02-13 18:18:11 +0200

    Delete routes.js

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import AppForm from './components/AppForm.vue';
-import Person from './components/Person.vue';
-
-export const routes = [
-  { path: '/', component: AppForm },
-  { path: '/person', component: Person }
-];
